fc32ee55d2c708a906b629a3a7d7fcba2c19d96a
Jon Daniel <binarycleric@gmail.com>
2019-06-01 18:58:16 -0700

    Debugging

--- app/controllers/stream_auth_controller.rb
+++ app/controllers/stream_auth_controller.rb
@@ -2,6 +2,8 @@ class StreamAuthController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :auth
 
   def auth
+    Rails.logger.info(params)
+
     if params[:name] == "jon" && params[:psk] == "password"
       render plain: "All Good", status: 201
     else
