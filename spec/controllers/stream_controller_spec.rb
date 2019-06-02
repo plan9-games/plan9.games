10018d1b04bd78baa014fd9d68699ea127c942ab
Jon Daniel <binarycleric@gmail.com>
2019-06-02 13:52:03 -0700

    Adding more tests

--- spec/controllers/stream_controller_spec.rb
+++ spec/controllers/stream_controller_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe StreamController do
       post :auth, params: { name: "" }
       expect(response).to have_http_status(:not_found)
     end
+
+    it "returns 201 when provided with proper stream key" do
+      post :auth, params: { name: "jon_xxx1" }
+      expect(response).to have_http_status(:created)
+    end
   end
 end
