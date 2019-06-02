33a90f98ade69ca599da6e4f91111a43efc1febb
Jon Daniel <binarycleric@gmail.com>
2019-06-01 19:09:00 -0700

    Testing

--- app/controllers/stream_auth_controller.rb
+++ app/controllers/stream_auth_controller.rb
@@ -1,10 +1,15 @@
 class StreamAuthController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :auth
 
-  def auth
-    Rails.logger.info(params)
+=begin
 
-    if params[:name] == "jon" && params[:psk] == "password"
+Parameters: {"app"=>"live", "flashver"=>"FMLE/3.0 (compatible; FMSc/1.0)", "swfurl"=>"rtmp://localhost:1935/live", "tcurl"=>"rtmp://localhost:1935/live", "pageurl"=>"", "addr"=>"172.17.0.1", "clientid"=>"17", "call"=>"publish", "name"=>"test", "type"=>"live"}
+
+=end
+
+
+  def auth
+    if params[:name] == "test"
       render plain: "All Good", status: 201
     else
       render plain: "No", status: 404
