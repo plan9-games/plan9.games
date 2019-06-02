61d7ff320002f0b06a3445ff1bf011a780133832
Jon Daniel <binarycleric@gmail.com>
2019-06-01 19:12:17 -0700

    Starting the idea of stream keys

--- app/controllers/stream_auth_controller.rb
+++ app/controllers/stream_auth_controller.rb
@@ -3,13 +3,26 @@ class StreamAuthController < ApplicationController
 
 =begin
 
-Parameters: {"app"=>"live", "flashver"=>"FMLE/3.0 (compatible; FMSc/1.0)", "swfurl"=>"rtmp://localhost:1935/live", "tcurl"=>"rtmp://localhost:1935/live", "pageurl"=>"", "addr"=>"172.17.0.1", "clientid"=>"17", "call"=>"publish", "name"=>"test", "type"=>"live"}
+Parameters: {
+  "app"=>"live",
+  "flashver"=>"FMLE/3.0 (compatible; FMSc/1.0)",
+  "swfurl"=>"rtmp://localhost:1935/live",
+  "tcurl"=>"rtmp://localhost:1935/live",
+  "pageurl"=>"", "addr"=>"172.17.0.1",
+  "clientid"=>"17", "call"=>"publish",
+  "name"=>"test",
+  "type"=>"live"
+}
 
 =end
 
-
   def auth
-    if params[:name] == "test"
+    allowed = [
+      "jon_xxx1",
+      "emma_desu_butts",
+    ]
+
+    if allowed.include?(params[:name])
       render plain: "All Good", status: 201
     else
       render plain: "No", status: 404
